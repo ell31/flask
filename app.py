b5ae42530bf64fc6b0f6bea0fbb34e62242b15ff
si.jung <si.jung@aiware.co.kr>
2023-04-07 10:11:59 +0900

    RTMP 테스트 프로그램으로 변경

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
 
-@app.route("/")
+camera = cv2.VideoCapture("rtmp://210.99.70.120/live/cctv045.stream")
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
 def index():
-    return render_template('./index.html')
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
